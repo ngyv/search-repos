4255d1a8a5baa993e42707191a8108cb3a8c071f
ngyv <ngdotyvonne@gmail.com>
2017-03-04 21:21:40 +0800

    Lazy load image

--- src/components/SearchReposResult.js
+++ src/components/SearchReposResult.js
@@ -2,25 +2,33 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var classnames = require('classnames');
 var css = require('../styles/searchRepos.less');
+var LazyLoad = require('react-lazy-load').default;
 
+function constructLazyImg (src) {
+	return (
+		<LazyLoad height={100}>
+			<img src={src} className={classnames('img-responsive','center-block', css.resultOwnerImg)} />
+		</LazyLoad>
+	)
+}
 function contructResult (result) {
 	return (
 		<div key={result.id} className={classnames('col-xs-12', 'col-sm-10', 'col-sm-offset-1')}>
 			Owner: {result.owner.login}
-			<img src={result.owner.avatar_url} className={classnames('img-responsive','center-block', css.resultOwnerImg)} />
+			{constructLazyImg(result.owner.avatar_url)}
 			Repo Name: {result.name}
 		</div>
 	)
 }
 
 function constructAllResults (repos) {
-	return repos.map((repo) => contructResult(repo))
+	return repos.map(contructResult)
 }
 
 function SearchReposResult (props) {
 	return (
 		<div>
-			{constructAllResults(props.repos)}
+			{constructAllResults(props.repos )}
 		</div>
 	)
 }
